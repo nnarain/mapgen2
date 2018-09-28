75665aa95bfa56d87e730296f026584b6b4ea580
Natesh Narain <nnaraindev@gmail.com>
2018-09-27 21:45:42 -0400

    Handle map events in node editor

---
+++ src/noise_gen/noise_map_manager.cpp
@@ -0,0 +1,34 @@
+#include "noise_gen/noise_map_manager.h"
+
+NoiseMapManager::NoiseMapManager()
+{
+}
+
+void NoiseMapManager::createNoiseMap(const std::string& name)
+{
+    if (noise_maps_.find(name) == noise_maps_.end())
+    {
+        noise_maps_[name] = NoiseMap{};
+    }
+}
+
+void NoiseMapManager::removeNoiseMap(const std::string& name)
+{
+    if (noise_maps_.find(name) != noise_maps_.end())
+    {
+        noise_maps_.erase(name);
+    }
+}
+
+void NoiseMapManager::forEach(std::function<void(const std::string&, NoiseMap&)> fn)
+{
+    for (auto& pair : noise_maps_)
+    {
+        fn(pair.first, pair.second);
+    }
+}
+
+NoiseMap& NoiseMapManager::getNoiseMap(const std::string& name)
+{
+    return noise_maps_[name];
+}
